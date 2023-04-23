eae97b4c0bc93285fbec8951ff4a56ffb71dada2
Rafael Merlotto <rafaelmerlottogmail.com>
2023-04-23 22:43:12 +0200

    agg

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function calcoloImpasto() {
 
     form.addEventListener('submit', event => {
         event.preventDefault();
-        const quantPalline = document.querySelector('.quant').value;
-        const pesoPallina = document.querySelector('.peso').value;
+        const quantPanetti = document.querySelector('.quant').value;
+        const pesoPanetto = document.querySelector('.peso').value;
         const idratazione = document.querySelector('.idratazione').value;
 
 
-        let calcoloIniziale = (quantPalline * pesoPallina);
+        let calcoloIniziale = (quantPanetti * pesoPanetto);
         let calcoloFarina = (calcoloIniziale * idratazione) / 100;
         let calcoloIdratazione = calcoloIniziale - calcoloFarina;
         let calcoloSale = ((calcoloIniziale * 2) / 100);
-        let calcoloLievito = (calcoloIniziale * 0.1) / 100;
+        let calcoloLievito = (calcoloIniziale * 0.2) / 100;
         let calcoloFinale = calcoloFarina + calcoloIdratazione + calcoloSale + calcoloLievito;
 
         const risultato = document.querySelector('.risultato');
@@ -22,7 +22,7 @@ function calcoloImpasto() {
                                <p>Acqua: ${Math.trunc(calcoloIdratazione).toFixed()}g</p><br> 
                                <p>Sale: ${Math.trunc(calcoloSale).toFixed()}g</p><br> 
                                <p>Lievito: ${calcoloLievito}g </p><br> 
-                               <p> Totale di ${quantPalline} palline da ${(calcoloFinale / quantPalline).toFixed()}g, ${idratazione}% di idratazione</p><br> `
+                               <p> Totale di ${quantPanetti} palline da ${(calcoloFinale / quantPanetti).toFixed()}g, ${idratazione}% di idratazione</p><br> `
     })
 
 }
